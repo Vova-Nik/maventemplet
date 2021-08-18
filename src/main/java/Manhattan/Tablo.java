2570deec5d7ab947a6ba31bf15a177102e63bb49
Vova-Nik <vmaseich@gmail.com>
2021-08-18 15:39:46 +0300

    Manhattan finished

--- src/main/java/Manhattan/Tablo.java
+++ src/main/java/Manhattan/Tablo.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Tablo {
     static String matrix;
-    static String tmpl = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?"; //l=27
+    static String tmpl = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
 
-    int L=4;
-    int H=5;
+    int L;
+    int H;
     int matrixOfset;
 
     char[] in;
@@ -15,63 +15,57 @@ public class Tablo {
     int outLen;
     int outOfset;
 
-    public Tablo(int L, int H, String tmpl) {
-            StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < tmpl.length(); i++) {
-
-            if(i%(L*27-1)==0 && i!=0) sb.append(' ');
-            sb.append(tmpl.charAt(i));
+    public Tablo(int L, int H, String inpMatrix) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < inpMatrix.length(); i++) {
+            if (i % (L * 27 - 1) == 0 && i != 0) sb.append(' ');
+            sb.append(inpMatrix.charAt(i));
         }
-        matrix=sb.toString();
-         this.L=L;
-         this.H=H;
-        matrixOfset = L*27;
-
+        sb.append(' ');
+        matrix = sb.toString();
+        System.out.println("matrix length = " + matrix.length());
+        this.L = L;
+        this.H = H;
+        matrixOfset = L * 27;
     }
 
 
     public String recode(String toRecode) {
         in = toRecode.toCharArray();
-
-        out =new char[H*L*in.length];
-        in = toRecode.toCharArray();
+        out = new char[H * L * in.length];
         outLen = toRecode.length() * L * H;
         out = new char[outLen];
-        outOfset = outLen/H;
+        outOfset = outLen / H;
 
         for (int i = 0; i < toRecode.length(); i++) {
-            if (tmpl.indexOf(in[i])<0){
+            if (tmpl.indexOf(in[i]) < 0) {
                 in[i] = '?';
             }
         }
-
-        //matrixOfset = 108;
-        //outOfset outLen/H;
+        toRecode = new String(in);
         for (int l = 0; l < in.length; l++) {
-            int matrixIndex = matrix.indexOf(toRecode.charAt(l));
-            int matrixAdress = matrixIndex*L;
-            int outAdress = l*L;
-
-            for (int i = 0; i < H; i++) {
-                for (int j = 0; j < L; j++) {
-                    int iii = matrixAdress + i * matrixOfset + j;
-                    char r = matrix.charAt(iii);
-
-                    int outInd = j + i * outOfset;
-                    out[outInd] = r;
+            int matrixAdress = tmpl.indexOf(toRecode.charAt(l)) * L;
+            int outAdress = l * L;
+            int x = 0, ym = 0, yo=0;
+            for (int i = 0; i < H * L; i++) {
+                if (i % L == 0 && i!=0) {
+                    ym += matrixOfset;
+                    yo += outOfset;
+                    x=0;
                 }
+                out[outAdress + x + yo] = matrix.charAt(matrixAdress + x + ym);
+                x++;
             }
+
         }
         return new String(out);
     }
 
-    public String getMatrix(){
+    public String getMatrix() {
         return matrix;
     }
-    public String getOut(){
+
+    public String getOut() {
         return new String(out);
     }
-
-//public char getNextSymbol()
-
 }
--- src/test/java/Manhattan/TabloTest.java
+++ src/test/java/Manhattan/TabloTest.java
@@ -10,32 +10,45 @@ class TabloTest {
 
     //                  0         1         2         3         4         5         6         7         8         8         1
     //                  01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456
-    static String s0 = " 1  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ###";
-    static String s1 = "2 3 # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   #";
-    static String s2 = "456 ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ##";
-    static String s3 = "7 8 # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #      ";
-    static String s4 = "9 0 ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  # ";
+//    static String s0 = "a1b ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ###";
+//    static String s1 = "2c3 # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   #";
+//    static String s2 = "456 ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ##";
+//    static String s3 = "7d8 # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #      ";
+//    static String s4 = "9e0 ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  # ";
+
+    static String s0 = " #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ###";
+    static String s1 = "# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   #";
+    static String s2 = "### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ##";
+    static String s3 = "# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #      ";
+    static String s4 = "# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  # ";
     static String grids = s0+s1+s2+s3+s4;
     static Tablo tablo;
 
     @BeforeAll
     static  void init(){
         tablo = new Tablo(4,5,grids);
-        System.out.println(tablo.getMatrix());
+//        System.out.println(tablo.getMatrix());
     }
 
     @Test
     void recode1() {
         String res = tablo.recode("A");
-        System.out.println(res);
         show1(res);
+        res = tablo.recode("F");
+        show1(res);
+        res = tablo.recode("*");
+        show1(res);
+
     }
 
     @Test
     void recode2() {
-        String res = tablo.recode("AB");
-        System.out.println(res);
-        show2(res);
+        String res = tablo.recode("MANHATTAN");
+        show(res);
+        assertTrue(res.contains("### # # # # # # # #  #   #  # # # #"));
+        res = tablo.recode("VOVA");
+        assertTrue(res.contains("#   #   #  # #"));
+        show(res);
     }
 
     void show1(String response){
@@ -47,10 +60,13 @@ class TabloTest {
         System.out.println(sb.toString());
     }
 
-    void show2(String response){
+    void show(String response){
+        int len = response.length();
+        int strlen = len/5;
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 40; i++) {
-            if(i%8 == 0 && i!=0)sb.append('\n');
+        for (int i = 0; i < len; i++) {
+            if(i%strlen == 0 && i!=0)sb.append('\n');
             sb.append(response.charAt(i));
         }
         System.out.println(sb.toString());

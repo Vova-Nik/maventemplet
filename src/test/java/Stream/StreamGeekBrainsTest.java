fe06bbb2a034d85c7f44c751dca42684bdbacdcc
Vova-Nik <vmaseich@gmail.com>
2021-09-21 06:31:37 +0300

    ComplitableFuture some more met added

--- src/test/java/Stream/StreamGeekBrainsTest.java
+++ src/test/java/Stream/StreamGeekBrainsTest.java
@@ -10,7 +10,7 @@ class StreamGeekBrainsTest {
 
     @Test
     void run() {
-        StreamGeekBrains sgb=new StreamGeekBrains();
+        StreamGeekBrains sgb = new StreamGeekBrains();
         sgb.run();
         List<Foo> foos = sgb.getFoos();
 //        System.out.println(foos);
